1f6f825df3a24d24c38ec033554e087c474821b9
Bjørn Kristian Strand <bjorkst@stud.ntnu.no>
2023-12-04 21:33:31 +0100

    fjernet varsel for fulladet batteri, unødvendig imo

--- bin/batteri/batterynotify.sh
+++ bin/batteri/batterynotify.sh
@@ -13,21 +13,14 @@ BATTERY_LEVEL=$(acpi -b | grep "Battery 0" | grep -P -o '[0-9]+(?=%)')
 
 # Use two files to store whether we've shown a notification or not (to prevent multiple notifications)
 EMPTY_FILE=/tmp/batteryempty
-FULL_FILE=/tmp/batteryfull
 
 # Reset notifications if the computer is charging/discharging
-if [ "$BATTERY_DISCHARGING" -eq 1 ] && [ -f $FULL_FILE ]; then
-    rm $FULL_FILE
-elif [ "$BATTERY_DISCHARGING" -eq 0 ] && [ -f $EMPTY_FILE ]; then
+if [ "$BATTERY_DISCHARGING" -eq 0 ] && [ -f $EMPTY_FILE ]; then
     rm $EMPTY_FILE
 fi
 
-# If the battery is charging and is full (and has not shown notification yet)
-if [ "$BATTERY_LEVEL" -gt 95 ] && [ "$BATTERY_DISCHARGING" -eq 0 ] && [ ! -f $FULL_FILE ]; then
-    notify-send "Battery Charged" "Battery is fully charged."
-    touch $FULL_FILE
 # If the battery is low and is not charging (and has not shown notification yet)
-elif [ "$BATTERY_LEVEL" -le $WARNING_LEVEL ] && [ "$BATTERY_DISCHARGING" -eq 1 ] && [ ! -f $EMPTY_FILE ]; then
+if [ "$BATTERY_LEVEL" -le $WARNING_LEVEL ] && [ "$BATTERY_DISCHARGING" -eq 1 ] && [ ! -f $EMPTY_FILE ]; then
     notify-send "Low Battery" "${BATTERY_LEVEL}% battery remaining." -u critical
     touch $EMPTY_FILE
 fi
