9ba22610c4a6b2283e24506cd5051f78f0baec73
Bjørn Kristian Strand <bjorkst@stud.ntnu.no>
2024-01-21 22:20:24 +0100

    fikset symlinks m/ shellscript, basic endringer i i3

---
+++ bin/symlinks.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# lager symlinks for dotfiles
+
+# bash
+ln -s -f /home/$USER/git/dotfiles/bash/.bashrc /home/$USER/
+ln -s -f /home/$USER/git/dotfiles/bash/.bash_aliases /home/$USER/
+
+# .config
+ln -s -f /home/$USER/git/dotfiles/.config/alacritty/alacritty.toml /home/$USER/.config/alacritty/
+ln -s -f /home/$USER/git/dotfiles/.config/flameshot/flameshot.ini /home/$USER/.config/flameshot/
+ln -s -f /home/$USER/git/dotfiles/.config/i3/config /home/$USER/.config/i3/
+ln -s -f /home/$USER/git/dotfiles/.config/i3status/config /home/$USER/.config/i3status/
+ln -s -f /home/$USER/git/dotfiles/.config/terminator/config /home/$USER/.config/terminator/
