07ad209b2f7c99fed5d07fa0c373c8084a9777e1
Bjørn Kristian Strand <bjorkst@stud.ntnu.no>
2024-01-23 18:09:25 +0100

    lagt til symlink i bin

--- bin/symlinks.sh
+++ bin/symlinks.sh
@@ -8,7 +8,7 @@ ln -s -f /home/$USER/git/dotfiles/bash/.bash_aliases /home/$USER/
 
 # .config
 ln -s -f /home/$USER/git/dotfiles/.config/alacritty/alacritty.toml /home/$USER/.config/alacritty/
-ln -s -f /home/$USER/git/dotfiles/.config/alacritty/test.alacritty.yml /home/$USER/.config/alacritty/
+ln -s -f /home/$USER/git/dotfiles/.config/alacritty/alacritty.toml /home/$USER/.config/alacritty/
 ln -s -f /home/$USER/git/dotfiles/.config/flameshot/flameshot.ini /home/$USER/.config/flameshot/
 ln -s -f /home/$USER/git/dotfiles/.config/i3/config /home/$USER/.config/i3/
 ln -s -f /home/$USER/git/dotfiles/.config/i3status/config /home/$USER/.config/i3status/
