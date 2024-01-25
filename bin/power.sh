f41f2e10050f7caa10d9502dfa262176e0c7f671
Bjørn Kristian Strand <bjorkst@stud.ntnu.no>
2024-01-25 21:37:01 +0100

    lagt til keybindings for suspending, reboot osv osv

---
+++ bin/power.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+
+case "$1" in
+    suspend)
+        systemctl suspend
+        ;;
+    hibernate)
+        systemctl hibernate
+        ;;
+    reboot)
+        systemctl reboot
+        ;;
+    shutdown)
+        systemctl poweroff
+        ;;
+    *)
+        echo "Usage: $0 [suspend|hibernate|reboot|shutdown]"
+        exit 2
+esac
+
+exit 0
