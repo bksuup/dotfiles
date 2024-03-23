a753cda394fb47f73dd201e546e51a06a77067a8
Bjørn Kristian Strand <bjorkst@stud.ntnu.no>
2024-03-23 21:38:04 +0100

    nvim bs

---
+++ .config/nvim/README.md
@@ -0,0 +1,7 @@
+# README NVIM Config
+
+## Todo:
+
+- [ ] Treesitter config, what does it do?
+- [ ] Harpoon, what it do?
+- [ ] LSP
---
+++ .config/nvim/lua/bksuup/lazy/one-dark.lua
@@ -0,0 +1,11 @@
+return {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+        require("rose-pine").setup({
+            variant = main,
+
+        })
+        vim.cmd("colorscheme rose-pine")
+    end,
+}
