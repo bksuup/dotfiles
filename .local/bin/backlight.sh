04a2e40cdbff8079c8444cd0ed6d3d12d2cce697
Bjørn Kristian Strand <bjorkst@stud.ntnu.no>
2024-05-20 22:38:50 +0200

    Satt tray_output til primary++

--- .local/bin/backlight.sh
+++ .local/bin/backlight.sh
@@ -2,7 +2,7 @@
 
 CURRENTLEVEL=$(brightnessctl --device='platform::kbd_backlight' info | grep -i "current brightness: " | awk -F " " '{print $3}')
 
-if [ $CURRENTLEVEL = 2 ]; then
+if [ $CURRENTLEVEL = 1 ]; then
   brightnessctl --device='platform::kbd_backlight' set 0
   notify-send "Backlight" "Turned Off"
 elif [ $CURRENTLEVEL = 0 ]; then
