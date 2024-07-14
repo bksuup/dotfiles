ac197e8c97fa117a032939389831bdd5c87febb9
Bjørn Kristian Strand <bjorkst@stud.ntnu.no>
2024-07-14 17:31:06 +0200

    Småendringer - Conflict løsing

--- .local/bin/backlight.sh
+++ .local/bin/backlight.sh
@@ -2,10 +2,13 @@
 
 CURRENTLEVEL=$(brightnessctl --device='platform::kbd_backlight' info | grep -i "current brightness: " | awk -F " " '{print $3}')
 
-if [ $CURRENTLEVEL = 1 ]; then
+if [ $CURRENTLEVEL = 2 ]; then
   brightnessctl --device='platform::kbd_backlight' set 0
   notify-send "Backlight" "Turned Off"
+elif [ $CURRENTLEVEL = 1 ]; then
+  brightnessctl --device='platform::kbd_backlight' set 2
+  notify-send "Backlight" "High"
 elif [ $CURRENTLEVEL = 0 ]; then
   brightnessctl --device='platform::kbd_backlight' set 1
-  notify-send "Backlight" "Turned On"
+  notify-send "Backlight" "Low"
 fi
