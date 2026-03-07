8794c61dae403beb11a280df9628bd332c7787c7
Bjørn Kristian Strand <bjkristian@proton.me>
2026-03-07 12:30:38 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# if we don't make this directory before symlinking
+# all files we add to ~/.local/bin (including ohmyposh binary)
+# will be added to the git-repo
+mkdir -p ~/.local/bin
+
+# Remove unwanted packages installed by default ###
+sudo pacman -R epiphany
+
+# install yay
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay makepkg -si
